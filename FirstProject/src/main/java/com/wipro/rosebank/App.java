0cda315ca1bba863ae810a63b43c721e22c239b0
Anirbany <royanirban2627@gmail.com>
2017-05-06 15:27:10 +0200

    changes

---
+++ FirstProject/src/main/java/com/wipro/rosebank/App.java
@@ -0,0 +1,24 @@
+package com.wipro.rosebank;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+       // System.out.println( "Hello World!" );
+        
+        System.out.println("Addition: "+add(1,3));
+        System.out.println("Addition: "+add(1,3));
+        
+        
+    }
+    
+    public static int add(int a,int b)
+    {
+    	
+    	return a+b;
+    }
+}
